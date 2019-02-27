83f276510619dc33ad3e2009956031099dfb4d89
Ethan <kkojaeh@gmail.com>
2019-02-27 17:37:34 +0900

    오류 수정

--- src/main/java/pico/erp/restapi/config/FaxConfiguration.java
+++ src/main/java/pico/erp/restapi/config/FaxConfiguration.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.val;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,7 +49,7 @@ public class FaxConfiguration {
 
   @Public
   @Bean
-  @ConditionalOnMissingBean(FaxExecuteService.class)
+  @Profile({"!production", "!development"})
   public FaxExecuteService noOpFaxExecuteService() {
     return new FaxExecuteService() {
 
